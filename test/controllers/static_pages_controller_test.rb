2c848a43b4cddf60d05642802138aa7e9c166383
leslie kimm <leslie.kimm@gmail.com>
2016-01-26 15:35:13 -0800

    Add contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Help | #{@base_title}"
   end
 
+  test "should get contact" do
+  	get :contact
+  	assert_response :success
+  	assert_select "title", "Contact | #{@base_title}"
+  end
+
 end
