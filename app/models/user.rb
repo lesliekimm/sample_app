2e81586130dceb3320dd4ebf5d62409f5062713a
leslie kimm <leslie.kimm@gmail.com>
2016-01-29 17:08:39 -0800

    Add two migrations for PW & email index Verify email is unique hash PWs for security

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
-	validates :email, presence: true, length: { maximum: 255 }
+	VALID_EMAIL_REGEX = /(?!.*\.{2})\A[\w+\-.]+@[a-z\d\-.]+\.[a-z][a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password, length: { minimum: 6 }, presence: true
 end
