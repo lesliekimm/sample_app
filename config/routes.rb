ca2ba67d2cbfaa3df064a5c212190ae25dcf90ef
leslie kimm <leslie.kimm@gmail.com>
2016-02-05 08:51:29 -0800

    Handle login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-    # You can have the root of your site routed with "root"
     root                'static_pages#home'
     get    'help'    => 'static_pages#help'
     get    'about'   => 'static_pages#about'
@@ -10,7 +7,6 @@ Rails.application.routes.draw do
     get    'login'   => 'sessions#new'
     post   'login'   => 'sessions#create'
     delete 'logout'  => 'sessions#destroy'
-
     resources :users
 
     # The priority is based upon order of creation: first created -> highest priority.
